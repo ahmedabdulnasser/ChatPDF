b2d7ef5668e67f36ead41c2bd65752d9d9eb61fd
Ahmed Abdel-Nasser <ahmedabdulnasser.ai@gmail.com>
2024-02-18 17:31:41 +0200

    Updated LogIn, SignUp and NotFound components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
-import {  RouterProvider, createBrowserRouter } from "react-router-dom"
-import LogIn from "./Pages/LogIn"
-import Home from "./Pages/Home"
-import NotFound from "./Pages/NotFound"
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
+import LogIn from "./Pages/LogIn";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import SignUp from "./Pages/SignUp";
 
-
-let routers = createBrowserRouter([{
-  path: "", element: <Home />,
-},
-{ path: "login", element: <LogIn /> },
-{ path: "*", element: <NotFound /> }
-])
-
+let routers = createBrowserRouter([
+  {
+    path: "",
+    element: <Home />,
+  },
+  { path: "login", element: <LogIn /> },
+  { path: "signup", element: <SignUp /> },
+  { path: "*", element: <NotFound /> },
+]);
 
 function App() {
-
-
   return (
     <>
       <RouterProvider router={routers} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,8 +1,32 @@
-
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 export default function NotFound() {
   return (
-    <div>
-          NotFound
-    </div>
-  )
+    <section className="w-screen h-screen flex justify-center items-center bg-gradient-to-r from-rose-400 via-fuchsia-500 to-indigo-500">
+      <div className="rounded-3xl bg-white drop-shadow">
+        <Link to="/">
+          <div className="flex">
+            <FontAwesomeIcon
+              icon={faArrowLeft}
+              className="relative left-4 top-4 text-rose-600"
+            />{" "}
+            <span className="relative text-sm left-[1.4rem] top-[0.88rem] font-cairo text-slate-500">
+              Go Home
+            </span>
+          </div>
+        </Link>
+
+        <div className="w-[32rem] h-[24rem] flex flex-col justify-center items-center">
+          <div className="text-8xl font-cairo text-rose-600">404</div>
+          <p className="text-2xl font-cairo text-rose-600">
+            Oopsie! Page not found.
+          </p>
+          <p className="text-lg italic font-mono mt-2 text-center">
+            The page you are looking for does not exist.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
